570528cfeece7fc2219ec61fa003ac5e7b67f5e3
Isaac <zackhyou@gmail.com>
2024-09-16 11:33:59 -0300

    💡 - comentários

--- popup/popUp.js
+++ popup/popUp.js
@@ -1,9 +1,9 @@
-let saltoS = document.getElementById("salto"); 
+let saltoS = document.getElementById("salto"); // definir uma variável para o elemento com ID 'salto'
 
-function saltar () { 
+function saltar () { // adicionar os estilos da classe 'animação' (efeitos visuais do popup)
     saltoS.classList.add("animacao");
 }
 
 function voltar () {
-    saltoS.classList.remove("animacao"); 
+    saltoS.classList.remove("animacao"); // remove a classe 'animacao'
 }
