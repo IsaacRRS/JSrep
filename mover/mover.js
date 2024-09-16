fd57d1eb1b55d900125e38516770351fdba726dc
Isaac <zackhyou@gmail.com>
2024-09-16 07:54:45 -0300

    💡 - comentários

--- mover/mover.js
+++ mover/mover.js
@@ -1,35 +1,35 @@
-let escolhido = null; 
+let escolhido = null; // variável para armazenar item arrastado
 
-document.querySelectorAll('.item').forEach(item => {
+document.querySelectorAll('.item').forEach(item => { // evento dragstart à classe 'item'
 
-    item.addEventListener('dragstart', function(e) {
+    item.addEventListener('dragstart', function(e) { // aciona quando um item for arrastado
 
-        escolhido = e.target;
+        escolhido = e.target; // armazena o item arrastado 
         
     });
 
 });
 
-function Arrastar(e) {
+function Arrastar(e) { // função chamada quando o item é arrastado 
 
-    e.preventDefault();
+    e.preventDefault(); // previnir o padrão de 'dragover' para permitir que o item seja solto
 
 }
 
-function Soltar(e) {
+function Soltar(e) { // função chamada quando o item é solto
 
-    e.preventDefault();
+    e.preventDefault(); // previnir o padrão de 'drop'
 
-    if (escolhido) {
+    if (escolhido) { // caso o item não for nulo (se há item arrastado)
 
-    e.target.appendChild(escolhido);
-    escolhido = null;
+    e.target.appendChild(escolhido); // adiciona o item ao alvo 
+    escolhido = null; // limpa a variável para que um próximo item possa ser arrastado
 
     }
 
 }
 
-  
+// a classe 'soltar' recebe 'dragover' e 'drop'
 document.querySelectorAll('.soltar').forEach(caixa => {
 
     caixa.addEventListener('dragover', Arrastar);
