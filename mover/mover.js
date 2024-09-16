8684883054506097c6097a63ca9c1a664bbfbf60
Isaac <zackhyou@gmail.com>
2024-09-16 07:48:17 -0300

    🚀 - funções js

--- mover/mover.js
+++ mover/mover.js
@@ -10,3 +10,30 @@ document.querySelectorAll('.item').forEach(item => {
 
 });
 
+function Arrastar(e) {
+
+    e.preventDefault();
+
+}
+
+function Soltar(e) {
+
+    e.preventDefault();
+
+    if (escolhido) {
+
+    e.target.appendChild(escolhido);
+    escolhido = null;
+
+    }
+
+}
+
+  
+document.querySelectorAll('.soltar').forEach(caixa => {
+
+    caixa.addEventListener('dragover', Arrastar);
+
+    caixa.addEventListener('drop', Soltar);
+
+    });
