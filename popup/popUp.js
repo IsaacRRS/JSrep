6dcd198888d8a661447523a3ff6b5c1d5b2f32a5
Isaac <zackhyou@gmail.com>
2024-09-16 11:33:41 -0300

    🔧 - funções

---
+++ popup/popUp.js
@@ -0,0 +1,9 @@
+let saltoS = document.getElementById("salto"); 
+
+function saltar () { 
+    saltoS.classList.add("animacao");
+}
+
+function voltar () {
+    saltoS.classList.remove("animacao"); 
+}
