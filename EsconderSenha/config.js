6474de85023e204363d39a28c55c2682b8f31c8c
Isaac <zackhyou@gmail.com>
2024-09-17 08:03:41 -0300

    💡 - comentários

--- EsconderSenha/config.js
+++ EsconderSenha/config.js
@@ -1,18 +1,18 @@
-let icone = document.getElementById("iconeID");
-let senha = document.getElementById("senhaID");
+let icone = document.getElementById("iconeID");  // armazenar o elemento com ID 'iconeID'
+let senha = document.getElementById("senhaID");  // armazenar o elemento com ID 'senhaID'
 
-icone.onclick = function() {
+icone.onclick = function() { // assim que o ícone for clicado..
 
-    if(senha.type === "password") {
+    if(senha.type === "password") { // caso o tipo do elemento atribuído à variável 'senha' seja 'password' 
 
-        senha.type = "text";
-        icone.classList.remove("bi-eye-slash");
+        senha.type = "text"; // seu tipo é trocado por 'text', mostrando o conteúdo da senha
+        icone.classList.remove("bi-eye-slash"); // alteração de ícone 
         icone.classList.add("bi-eye");
 
     } else {
 
-        senha.type = "password";
-        icone.classList.remove("bi-eye");
+        senha.type = "password"; // caso o tipo já tenha sido mudado para 'text', voltará para 'password' caso seja novamente clicado
+        icone.classList.remove("bi-eye"); // alteração de ícone 
         icone.classList.add("bi-eye-slash");
         
     }
